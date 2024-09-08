148dab0a8173fa9b7687a11da1b7e743cee120cf
SkyN9ne <82126207+SkyN9ne@users.noreply.github.com>
2024-09-08 11:25:30 +0300

    Update lseek_compile.sh

--- lseek_builtin/lseek_compile.sh
+++ lseek_builtin/lseek_compile.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 git clone https://git.savannah.gnu.org/git/bash.git
 
 cd bash
@@ -13,7 +15,7 @@ curl -o ./lseek.c 'https://raw.githubusercontent.com/jkool702/forkrun/main/lseek
 
 gcc -v -fPIC -flto -DHAVE_CONFIG_H -DSHELL -DLOADABLE_BUILTIN -DSELECT_COMMAND -O3 -Wno-parentheses -Wno-format-security -I.  -I/usr/include/bash -I/usr/include/bash/builtins -I/usr/include/bash/include --shared -o lseek lseek.c;
 
-mkdir -p /usr/local/lib/bash
+mkdir -p -v /usr/local/lib/bash
 cp lseek /usr/local/lib/bash
 
 enable lseek
